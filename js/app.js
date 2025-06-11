e36dd3e244d2db6a913024adecc6ed04768b528b
Chris McGurn <214693239+cmcgurndev@users.noreply.github.com>
2025-06-11 03:15:19 +0000

    Add getter methods to Todo prototype

--- js/app.js
+++ js/app.js
@@ -6,12 +6,24 @@ function Todo(id, title, isDone) {
   this.isDone = isDone;
 }
 
+Todo.prototype.getId = function () {
+  return this.id;
+};
+
 Todo.prototype.setTitle = function (title) {
   if (!title) return;
   this.title = title;
 };
 
+Todo.prototype.getTitle = function () {
+  return this.title;
+};
+
 Todo.prototype.setIsDone = function (isDone) {
   if (!isDone || typeof isDone !== 'boolean') return;
   this.isDone = isDone;
+};
+
+Todo.prototype.getIsDone = function () {
+  return this.isDone;
 };
